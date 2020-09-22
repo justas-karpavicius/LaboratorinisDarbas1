4c95a64ddc53e1cad1609416cb385ee66afb5e5b
justas-karpavicius <45791871+justas-karpavicius@users.noreply.github.com>
2020-09-23 01:17:03 +0300

    Add project files.

---
+++ LaboratorinisDarbas1/InOutUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LaboratorinisDarbas1
+{
+	static class InOutUtils
+	{
+		public static List<Question> ReadQuestions(string fileName)
+		{
+			List<Question> Questions = new List<Question>();
+			string[] Lines = File.ReadAllLines(fileName, Encoding.UTF8);
+			foreach (string line in Lines) 
+			{
+				string[] Values = line.Split(';');
+				int  = Values.Length - 5;
+				List<string> possibleAnswers
+				Question question = new Question(Values[0], double.Parse(Values[1]), Values[2], Values[3],
+					CamountOfPA, possibleAnswers, int.Parse(Values[8]));
+				Questions.Add(question);
+			}
+			return Questions;
+		}
+	}
+}
---
+++ LaboratorinisDarbas1/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorinisDarbas1
+{
+	class Question
+	{
+		public string Topic { get; set; }
+		public double Difficulty { get; set; }
+		public string Author { get; set; }
+		public string QuestionText { get; set; }
+		public List<string> PossibleAnswers { get; set; }
+		public int IndexOfAnswer { get; set; }
+
+		public Question(string topic, double difficulty, string author, string questionText,
+			List<string> possibleAnswers, int indexOfAnswer)
+		{
+			this.Topic = topic;
+			this.Difficulty = difficulty;
+			this.Author = author;
+			this.QuestionText = questionText;
+			this.PossibleAnswers = possibleAnswers;
+			this.IndexOfAnswer = indexOfAnswer;
+		}
+	}
+}		
